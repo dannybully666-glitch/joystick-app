d22377afe67e88ecfd972a7cf668812693fb69bd
dannybully666-glitch <dannybully666@gmail.com>
2025-12-29 01:15:12 +0530

    Update MainActivity.kt

--- app/src/main/java/your/package/name/MainActivity.kt
+++ app/src/main/java/your/package/name/MainActivity.kt
@@ -1,7 +1,7 @@
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    Log.d("JoystickApp", "MainActivity loaded")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
