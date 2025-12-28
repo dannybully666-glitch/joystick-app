d6fbbd69270d4dd2ed6e5b3755fdfb7b4defe8c5
dannybully666-glitch <dannybully666@gmail.com>
2025-12-28 21:35:35 +0530

    gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,8 @@
-404: Not Found
+@echo off
+set DIRNAME=%~dp0
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+
+"%JAVA_HOME%\bin\java.exe" %DEFAULT_JVM_OPTS% -classpath "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
