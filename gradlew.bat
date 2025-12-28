79adb656bec6cb0a5275766bf3b7b4c62b0d7f57
dannybully666-glitch <dannybully666@gmail.com>
2025-12-28 21:57:21 +0530

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set DIRNAME=%~dp0
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
-
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
-
-"%JAVA_HOME%\bin\java.exe" %DEFAULT_JVM_OPTS% -classpath "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
