d9b353e13e5b1593c051a3a3ef29467e160af5b3
dannybully666-glitch <dannybully666@gmail.com>
2025-12-29 09:19:12 +0530

    Update MainActivity.kt

--- app/src/main/java/your/package/name/MainActivity.kt
+++ app/src/main/java/your/package/name/MainActivity.kt
@@ -1,3 +1,8 @@
+package com.joystickapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
